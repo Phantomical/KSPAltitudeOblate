50a435a2fde2b4495567e90c158d424245e10290
Sean Lynch <phantom@lynches.ca>
2026-01-30 16:44:31 -0800

    Add missing patches

---
+++ src/OblateAtmosphere/Patches/Vessel_getCorrectedLandedAltitude.cs
@@ -0,0 +1,29 @@
+using System;
+using HarmonyLib;
+
+namespace OblateAtmosphere.Patches;
+
+[HarmonyPatch(typeof(Vessel), "getCorrectedLandedAltitude")]
+public static class Vessel_getCorrectedLandedAltitude
+{
+    public static bool Prefix(
+        Vessel __instance,
+        double lat,
+        double lon,
+        double alt,
+        CelestialBody body,
+        ref double __result
+    )
+    {
+        if (body.scaledElipRadMult.z == 1.0)
+            return true;
+
+        Vector3d relSurfaceNVector = body.GetRelSurfaceNVector(lat, lon);
+        double surfaceHeight = body.pqsController.GetSurfaceHeight(relSurfaceNVector);
+        double latRad = lat * UtilMath.Deg2Rad;
+        double seaLevelR = OblateUtils.GetSeaLevelRadius(body, latRad);
+        double val = surfaceHeight - seaLevelR;
+        __result = Math.Max(alt, val);
+        return false;
+    }
+}
